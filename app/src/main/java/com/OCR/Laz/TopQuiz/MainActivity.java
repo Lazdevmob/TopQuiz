e8f13208e8d940181ecb30f10b9b1d5e82756244
Lazdevmob <laziz.saada@expleogroup.com>
2021-05-11 16:09:16 +0200

    commit 1

---
+++ app/src/main/java/com/OCR/Laz/TopQuiz/MainActivity.java
@@ -0,0 +1,13 @@
+package com.OCR.Laz.TopQuiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
