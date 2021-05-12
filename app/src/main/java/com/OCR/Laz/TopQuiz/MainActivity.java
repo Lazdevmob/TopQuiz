05c08eccfca93a662fe18f383ef5c23ab16a3230
Lazdevmob <laziz.saada@expleogroup.com>
2021-05-12 19:58:39 +0200

    commit manage user actions

--- app/src/main/java/com/OCR/Laz/TopQuiz/MainActivity.java
+++ app/src/main/java/com/OCR/Laz/TopQuiz/MainActivity.java
@@ -1,17 +1,20 @@
 package com.OCR.Laz.TopQuiz;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
     private TextView mGreetingText;
     private EditText mNameInput;
     private Button mPlayButton;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +24,32 @@ public class MainActivity extends AppCompatActivity {
         mGreetingText = (TextView) findViewById(R.id.activity_main_greeting_main);
         mNameInput = (EditText) findViewById(R.id.activity_main_name_input);
         mPlayButton = (Button) findViewById(R.id.acttivity_main_play_btn);
+
+        mPlayButton.setEnabled(false);
+
+        mNameInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // This is where we'll check the user input
+                mPlayButton.setEnabled(s.toString().length() != 0);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // The user just clicked
+            }
+        });
     }
 }
