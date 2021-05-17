83b14fc24cd22ee46129773486c1db53cfac85b2
Lazdevmob <laziz.saada@expleogroup.com>
2021-05-17 18:10:57 +0200

    commit add value & reference game_activity.java

--- app/src/main/java/com/OCR/Laz/TopQuiz/GameActivity.java
+++ app/src/main/java/com/OCR/Laz/TopQuiz/GameActivity.java
@@ -2,13 +2,39 @@ package com.OCR.Laz.TopQuiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
+
+   private TextView mQuestionText;
+   private Button mAnswer1;
+   private Button mAnswer2;
+   private Button mAnswer3;
+   private Button mAnswer4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        mQuestionText = (TextView) findViewById(R.id.activity_game_question_text);
+        mAnswer1 = (Button) findViewById(R.id.activity_game_answer1_btn);
+        mAnswer2 = (Button) findViewById(R.id.activity_game_answer2_btn);
+        mAnswer3 = (Button) findViewById(R.id.activity_game_answer3_btn);
+        mAnswer4 = (Button) findViewById(R.id.activity_game_answer4_btn);
+
+        /**
+        mAnswer1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // The user just clicked
+                Intent gamebackActivityIntent = new Intent(GameActivity.this,MainActivity.class);
+                startActivity(gamebackActivityIntent);
+            }
+        });
+*/
     }
 }
